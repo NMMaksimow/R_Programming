0baba07aee07cec0f4b6e775830b8a280c289e33
NMMaksimow <nmmaksimow@gmail.com>
2024-02-11 23:15:47 +0000

    swirl file instead 1_week

---
+++ swirl.R
@@ -0,0 +1,33 @@
+#Settings & sessionInfo
+
+?version
+sessionInfo()
+getwd()
+Sys.setenv(LANG = "en")
+Sys.getenv()
+
+#Different kinds of objects
+
+a <- c(1, 4, 8)
+b <- vector()
+c <- a*b
+x <- 1/Inf
+
+#Swirl
+install.packages("swirl")
+packageVersion("swirl")
+library(swirl)
+ls()
+rm(list=ls())
+
+swirl()
+install_from_swirl("R Programming")
+
+#R is one-based indexing language NB!
+
+#Operations on directories
+?dir.exists()
+?dir.create()
+?dir()
+?unlink()
+?list.dirs()
